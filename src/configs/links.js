3fd13f4c36b4eb6233c4ccd7d28af64df6e859f9
AlekseiMakhov <66823049+AlekseiMakhov@users.noreply.github.com>
2021-03-12 00:07:56 +0300

    err

---
+++ src/configs/links.js
@@ -0,0 +1,11 @@
+export const praktikumLink = 'https://praktikum.yandex.ru/';
+export const githubLink = 'https://github.com/';
+export const facebookLink = 'https://facebook.com/';
+
+export const staticSiteLink = 'https://alekseimakhov.github.io/russian-travel/index.html';
+export const adaptiveSiteLink = 'https://alekseimakhov.github.io/';
+export const oneSiteApp = 'https://alekseimakhov.github.io/turbinA/';
+
+export const myPhoto = '../../images/myPhoto.jpg';
+
+export const mainPageLink = '/';
